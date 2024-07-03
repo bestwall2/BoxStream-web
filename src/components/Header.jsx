98fb54d82250ca12e43035c700f4fa62890e00b3
Ahmed Dikha <85714557+bestwall2@users.noreply.github.com>
2024-07-03 13:24:16 +0100

    update src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,83 +1,102 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import InstallButton from './InstallButton';
+import React, { useRef, useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import InstallButton from "./InstallButton";
 
 export default function Header() {
-  const navigate = useNavigate();
-  const searchInputRef = useRef();
-  const [lastScrollTop, setLastScrollTop] = useState(0);
-  const [isVisible, setIsVisible] = useState(true);
-  const [isTop, setIsTop] = useState(true);
+    const navigate = useNavigate();
+    const searchInputRef = useRef();
+    const [lastScrollTop, setLastScrollTop] = useState(0);
+    const [isVisible, setIsVisible] = useState(true);
+    const [isTop, setIsTop] = useState(true);
 
-  function handleSearch() {
-    const query = searchInputRef.current.value;
-    if (query) {
-      navigate(`/search/${query}`);
+    function handleSearch() {
+        const query = searchInputRef.current.value;
+        if (query) {
+            navigate(`/search/${query}`);
+        }
     }
-  }
 
-  function handleKeyPress(e) {
-    if (e.key === 'Enter') {
-      handleSearch();
+    function handleKeyPress(e) {
+        if (e.key === "Enter") {
+            handleSearch();
+        }
     }
-  }
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      
-      if (currentScrollTop === 0) {
-        setIsTop(true);
-      } else {
-        setIsTop(false);
-      }
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollTop =
+                window.pageYOffset || document.documentElement.scrollTop;
 
-      if (currentScrollTop > lastScrollTop) {
-        setIsVisible(false);
-      } else {
-        setIsVisible(true);
-      }
-      setLastScrollTop(currentScrollTop <= 0 ? 0 : currentScrollTop);
-    };
+            if (currentScrollTop === 0) {
+                setIsTop(true);
+            } else {
+                setIsTop(false);
+            }
 
-    window.addEventListener('scroll', handleScroll);
+            if (currentScrollTop > lastScrollTop) {
+                setIsVisible(false);
+            } else {
+                setIsVisible(true);
+            }
+            setLastScrollTop(currentScrollTop <= 0 ? 0 : currentScrollTop);
+        };
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [lastScrollTop]);
+        window.addEventListener("scroll", handleScroll);
 
-  return (
-    <div id="header" className={`${isVisible ? 'visible' : 'show'} ${isTop ? 'no-filter' : 'with-filter'}`}>
-      <div id="header-section">
-        <div id="header-left">
-          <Link to="/">
-            <img src="/images/logo.svg" id="header-logo" alt="Logo"
-            style={{height: "90px"}} />
-          </Link>
-        </div>
-        <div id="header-right">
-          <InstallButton />
-          <div id="search">
-            <input
-              type="text"
-              id="search-input"
-              required
-              placeholder="Type to search..."
-              ref={searchInputRef}
-              onKeyPress={handleKeyPress}
-            />
-            <div className="icon" onClick={handleSearch}>
-              <i className="fa-regular fa-magnifying-glass" alt="Search" style={{fontSize: "25px", padding: "2px 0 0 2px"}} />
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [lastScrollTop]);
+
+    return (
+        <div
+            id="header"
+            className={`${isVisible ? "visible" : "show"} ${
+                isTop ? "no-filter" : "with-filter"
+            }`}>
+            <div id="header-section">
+                <div id="header-left">
+                    <Link to="/">
+                        <img
+                            src="/images/logo.svg"
+                            id="header-logo"
+                            alt="Logo"
+                        />
+                    </Link>
+                </div>
+                <div id="header-right">
+                    <InstallButton />
+                    <div id="search">
+                        <input
+                            type="text"
+                            id="search-input"
+                            required
+                            placeholder="Type to search..."
+                            ref={searchInputRef}
+                            onKeyPress={handleKeyPress}
+                        />
+                        <div className="icon" onClick={handleSearch}>
+                            <i
+                                className="fa-regular fa-magnifying-glass"
+                                alt="Search"
+                                style={{
+                                    fontSize: "25px",
+                                    padding: "2px 0 0 2px"
+                                }}
+                            />
+                        </div>
+                    </div>
+                    <button id="settings">
+                        <Link to="/settings">
+                            <i
+                                className="fa-light fa-gear"
+                                alt="Settings"
+                                style={{ fontSize: "30px" }}
+                            />
+                        </Link>
+                    </button>
+                </div>
             </div>
-          </div>
-          <button id="settings">
-            <Link to="/settings">
-              <i className="fa-light fa-gear" alt="Settings" style={{ fontSize: "30px" }} />
-            </Link>
-          </button>
         </div>
-      </div>
-    </div>
-  );
-};
+    );
+}
