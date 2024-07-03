c7df91f0c99c38b8c8ba847bb6ed429d5e354250
Ahmed Dikha <85714557+bestwall2@users.noreply.github.com>
2024-07-03 13:25:18 +0100

    update src/components/Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,11 +9,11 @@ export default function Footer() {
                 <div id="footer-logo"><img src="/images/logo.svg" style={{width: "90px"}} alt="Logo"/></div>
             </div>
             <div id="message"><p>This site does not store any files on our server, we only link to the media which is hosted on 3rd party services.</p></div>
-            <div id="copyright">Copyright © zmov 2024</div>
+            <div id="copyright">Copyright © BoxStream 2024</div>
         </div>
         <div id="footer-right">
             <Link to="/dmca" id="dmca">DMCA</Link>
-            <a target="_blank" href="https://github.com/coen-h" rel="noopener noreferrer"><i className="fa-brands fa-github" style={{color: "#ffffff"}} id="github" alt="Github Icon"></i></a>
+            <a target="_blank" href="https://bestwall2.github.io/BOXSTREAM/" rel="noopener noreferrer"><i className="fa-brands fa-github" style={{color: "#ffffff"}} id="github" alt="Github Icon"></i></a>
         </div>
     </div>
     )
