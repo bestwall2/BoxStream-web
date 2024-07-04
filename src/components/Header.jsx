71c485ff53b65c61f1d7a1c874a541a81febca96
Ahmed Dikha <85714557+bestwall2@users.noreply.github.com>
2024-07-04 21:25:14 +0100

    update src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -56,13 +56,6 @@ export default function Header() {
             }`}>
             <div id="header-section">
                 <div id="header-left">
-                    <a href="https://t.me/box_stream">
-                        <img
-                            src="/images/telegram.svg"
-                            id="header-tele"
-                            alt="Telegram Channel"
-                        />
-                    </a>
                     <Link to="/">
                         <img
                             src="/images/logo.svg"
