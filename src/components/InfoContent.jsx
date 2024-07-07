3d4d8cb34f4f81b080816e1d75b03034ff95413c
Ahmed Dikha <85714557+bestwall2@users.noreply.github.com>
2024-07-07 11:36:21 +0100

    update src/components/InfoContent.jsx

--- src/components/InfoContent.jsx
+++ src/components/InfoContent.jsx
@@ -149,7 +149,7 @@ export default function InfoContent() {
         <>
           <div id="info-container">
             <img id="info-backdrop" src={data.item.backdrop_path && `https://image.tmdb.org/t/p/original${data.item.backdrop_path}`} alt="Backdrop" />
-            <img id="info-poster" src={data.item.poster_path && `https://image.tmdb.org/t/p/w500/${data.item.poster_path}`} alt="Poster" />
+            <img id="info-poster" src={data.item.poster_path && `https://image.tmdb.org/t/p/w500${data.item.poster_path}`} alt="Poster" />
             <div id="info-top">
               <img id="info-title" src={data.logoImage && `https://image.tmdb.org/t/p/w500${data.logoImage}`} alt={type === 'movie' ? data.item.title : data.item.name} />
               <p id="info-title-text">{type === 'movie' ? data.item.title : data.item.name}</p>
